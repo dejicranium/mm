e32527228ce95f7c601a02b5f23d7b538736f572
silasdahun <98063668+Oladipopreserve-19@users.noreply.github.com>
2023-02-02 21:10:45 +0100

    wait it gonna work

---
+++ script/popup.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".get").click(function () {
+    $(".modal-box").toggleClass("show-modal");
+    $(".get").toggleClass("show-modal");
+  });
+  $(".fa-times").click(function () {
+    $(".modal-box").toggleClass("show-modal");
+    $(".get").toggleClass("show-modal");
+  });
+});

8a757a25588c0ea6ba47d50643351d6c463eed1e
silasdahun <98063668+Oladipopreserve-19@users.noreply.github.com>
2023-03-02 03:41:34 +0100

    .

--- script/article.js
+++ script/article.js
@@ -11,24 +11,3 @@ if (window.netlifyIdentity) {
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
-var listItems = document.getElementById("slides-container-li").children;
-
-let currentIndex = 0;
-
-function clickNext() {
-  //console.log(currentIndex >= listItems.length - 1);
-  if (currentIndex >= listItems.length - 1) {
-    currentIndex = -1;
-  }
-  [...listItems].forEach((item) => (item.style.display = "none"));
-  listItems[++currentIndex].style.display = "inline";
-  //console.log('after increment', currentIndex)
-}
-
-function clickPrevious() {
-  if (currentIndex <= 0) {
-    currentIndex = listItems.length;
-  }
-  [...listItems].forEach((item) => (item.style.display = "none"));
-  listItems[--currentIndex].style.display = "inline";
-}

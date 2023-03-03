e836ba5b1c4798d0d0088a55c9715b34dc73dc5f
silasdahun <98063668+Oladipopreserve-19@users.noreply.github.com>
2023-03-03 23:09:59 +0100

    .

--- script/article.js
+++ script/article.js
@@ -11,3 +11,22 @@ if (window.netlifyIdentity) {
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
+const searchButton = document.getElementById("search-button");
+const searchBox = document.getElementById("search-box");
+
+searchButton.addEventListener("click", () => {
+  const searchTerm = searchBox.value.toLowerCase();
+  const articles = document.querySelectorAll(".article-section");
+
+  articles.forEach((article) => {
+    const title = article
+      .querySelector(".product_name a")
+      .textContent.toLowerCase();
+
+    if (title.includes(searchTerm)) {
+      article.style.display = "block";
+    } else {
+      article.style.display = "none";
+    }
+  });
+});

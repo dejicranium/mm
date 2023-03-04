b40bc959461e0a211f4b1fdc8dee408ffe0e903a
silasdahun <98063668+Oladipopreserve-19@users.noreply.github.com>
2023-03-04 19:17:00 +0100

    .

--- script/article.js
+++ script/article.js
@@ -11,22 +11,35 @@ if (window.netlifyIdentity) {
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
-const searchButton = document.getElementById("search-button");
-const searchBox = document.getElementById("search-box");
-
-searchButton.addEventListener("click", () => {
-  const searchTerm = searchBox.value.toLowerCase();
-  const articles = document.querySelectorAll(".article-section");
-
-  articles.forEach((article) => {
-    const title = article
-      .querySelector(".product_name a")
-      .textContent.toLowerCase();
-
-    if (title.includes(searchTerm)) {
-      article.style.display = "block";
-    } else {
-      article.style.display = "none";
-    }
-  });
-});
+/////////////////////////////////////////SEARCH BAR FOR ARTICLE /////////////////////////////////
+const searchBox = document.querySelector(".search-box");
+const searchBtn = document.querySelector(".search-icon");
+const cancelBtn = document.querySelector(".cancel-icon");
+const searchInput = document.querySelector("input");
+const searchData = document.querySelector(".search-data");
+searchBtn.onclick = () => {
+  searchBox.classList.add("active");
+  searchBtn.classList.add("active");
+  searchInput.classList.add("active");
+  cancelBtn.classList.add("active");
+  searchInput.focus();
+  if (searchInput.value != "") {
+    var values = searchInput.value;
+    searchData.classList.remove("active");
+    searchData.innerHTML =
+      "You just typed " +
+      "<span style='font-weight: 500;'>" +
+      values +
+      "</span>";
+  } else {
+    searchData.textContent = "";
+  }
+};
+cancelBtn.onclick = () => {
+  searchBox.classList.remove("active");
+  searchBtn.classList.remove("active");
+  searchInput.classList.remove("active");
+  cancelBtn.classList.remove("active");
+  searchData.classList.toggle("active");
+  searchInput.value = "";
+};

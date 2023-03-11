51ba1a7288ebd26b40687ea55db0796ea6ba7592
silasdahun <98063668+Oladipopreserve-19@users.noreply.github.com>
2023-03-11 23:28:10 +0100

    preloader

--- script/navscript.js
+++ script/navscript.js
@@ -1,3 +1,16 @@
 $(".icon").click(function () {
   $("span").toggleClass("cancel");
 });
+
+//////////////////////
+//////preloader//////
+
+$(document).ready(function () {
+  //Preloader
+  preloaderFadeOutTime = 500;
+  function hidePreloader() {
+    var preloader = $(".spinner-wrapper");
+    preloader.fadeOut(preloaderFadeOutTime);
+  }
+  hidePreloader();
+});

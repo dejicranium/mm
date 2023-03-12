4e526ef90f3fe889998c33c718d67d4a83aab5c4
silasdahun <98063668+Oladipopreserve-19@users.noreply.github.com>
2023-03-12 17:47:04 +0100

    Update navscript.js

--- script/navscript.js
+++ script/navscript.js
@@ -5,16 +5,6 @@ $(".icon").click(function () {
 //////////////////////
 //////preloader//////
 
-$(document).ready(function () {
-  //Preloader
-  preloaderFadeOutTime = 500;
-  function hidePreloader() {
-    var preloader = $(".spinner-wrapper");
-    preloader.fadeOut(preloaderFadeOutTime);
-  }
-  hidePreloader();
-});
-
 var loader = document.getElementById("preloader");
 window.addEventListener("load", function () {
   loader.style.display = "none";

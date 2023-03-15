b029b5935a06c184f8db2bcf1ca2bb8d14b1ab4d
silasdahun <98063668+Oladipopreserve-19@users.noreply.github.com>
2023-03-15 01:55:32 +0100

    sticky nav bar done

--- script/navscript.js
+++ script/navscript.js
@@ -9,3 +9,17 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load", function () {
   loader.style.display = "none";
 });
+//////////////////////////////////
+////////////////////////////////////////nav sticky/////////////
+document.addEventListener("DOMContentLoaded", function () {
+  var nav = document.getElementById("nav");
+  var current_pos = nav.offsetTop;
+  window.onscroll = function () {
+    var window_pos = document.documentElement.scrollTop;
+    if (window_pos >= current_pos) {
+      nav.classList.add("sticky");
+    } else {
+      nav.classList.remove("sticky");
+    }
+  };
+});

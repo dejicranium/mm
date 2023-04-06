fb2464774bb833d2c37242e1b6877affb7adbe92
silasdahun <98063668+Oladipopreserve-19@users.noreply.github.com>
2023-04-06 23:35:10 +0100

    .

--- script/navscript.js
+++ script/navscript.js
@@ -13,13 +13,16 @@ window.addEventListener("load", function () {
 ////////////////////////////////////////nav sticky/////////////
 document.addEventListener("DOMContentLoaded", function () {
   var nav = document.getElementById("nav");
+  var logo = document.getElementById("logo");
   var current_pos = nav.offsetTop;
   window.onscroll = function () {
     var window_pos = document.documentElement.scrollTop;
     if (window_pos >= current_pos) {
       nav.classList.add("sticky");
+      logo.classList.add("nologo");
     } else {
       nav.classList.remove("sticky");
+      logo.classList.remove("nologo");
     }
   };
 });

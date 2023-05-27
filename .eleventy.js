06c8c0fcef8d6877de55d2786a14f042e9b25a91
silasdahun <98063668+Oladipopreserve-19@users.noreply.github.com>
2023-05-27 19:59:13 +0100

    .

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,9 @@ const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets/blog");
   eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("/src/assets");
   eleventyConfig.addPassthroughCopy("./src/admin");
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
